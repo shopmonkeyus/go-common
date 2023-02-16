58d05ec04e55e8df4a23edf46833c5efeae5a6f2
Jeff Haynie <jhaynie@shopmonkey.io>
2023-02-15 19:07:19 -0600

    Add common GCP and system helpers

---
+++ gcp/init.go
@@ -0,0 +1,24 @@
+package gcp
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"golang.org/x/oauth2/google"
+	compute "google.golang.org/api/compute/v1"
+)
+
+// FetchProjectID will return the GCP project id
+func FetchProjectID() (string, error) {
+	id := os.Getenv("PROJECT_ID")
+	if id != "" {
+		return id, nil
+	}
+	ctx := context.Background()
+	credentials, err := google.FindDefaultCredentials(ctx, compute.ComputeScope)
+	if err != nil {
+		return "", fmt.Errorf("error finding default google credentials: %s", err)
+	}
+	return credentials.ProjectID, nil
+}
