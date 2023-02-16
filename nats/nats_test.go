9b40631d38e4ed7056ae01f168138c505d67a9ac
Jeff Haynie <jhaynie@shopmonkey.io>
2023-02-15 20:45:45 -0600

    Add NATS exactly once handler, compression util (#5)

--- nats/nats_test.go
+++ nats/nats_test.go
@@ -1,7 +1,9 @@
 package nats
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/nats-io/nats-server/v2/server"
 	natsserver "github.com/nats-io/nats-server/v2/test"
@@ -10,15 +12,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func RunTestServer() *server.Server {
+func RunTestServer(js bool) *server.Server {
 	opts := natsserver.DefaultTestOptions
 	opts.Port = 8222
 	opts.Cluster.Name = "testing"
+	opts.JetStream = js
 	return natsserver.RunServer(&opts)
 }
 
 func TestNats(t *testing.T) {
-	server := RunTestServer()
+	server := RunTestServer(false)
 	log := logger.NewTestLogger()
 	n, err := NewNats(log, "test", "nats://localhost:8222", nil)
 	assert.NoError(t, err, "failed to connect to nats")
@@ -34,7 +37,7 @@ func TestNats(t *testing.T) {
 }
 
 func TestNatsWithOpts(t *testing.T) {
-	server := RunTestServer()
+	server := RunTestServer(false)
 	log := logger.NewTestLogger()
 	n, err := NewNats(log, "test", "nats://localhost:8222,nats://foo:9822,nats://bar:9100", nil, nats.DontRandomize())
 	assert.NoError(t, err, "failed to connect to nats")
@@ -47,4 +50,40 @@ func TestNatsWithOpts(t *testing.T) {
 	assert.Len(t, log.Logs[0].Arguments, 3)
 	assert.Equal(t, "nats://localhost:8222", log.Logs[0].Arguments[1])
 	assert.Len(t, log.Logs[0].Arguments[2], 56, "invalid nats id")
+}
+
+func TestExactlyOnceConsumer(t *testing.T) {
+	server := RunTestServer(true)
+	log := logger.NewTestLogger()
+	n, err := NewNats(log, "test", "nats://localhost:8222", nil)
+	assert.NoError(t, err, "failed to connect to nats")
+	assert.NotNil(t, n, "result was nil")
+	js, err := n.JetStream()
+	assert.NoError(t, err, "failed to create jetstream")
+	assert.NotNil(t, js, "js result was nil")
+	_, err = js.AddStream(&nats.StreamConfig{
+		Name:     "test",
+		Subjects: []string{"test.>"},
+	})
+	assert.NoError(t, err, "failed to create stream")
+	var received string
+	var msgid string
+	handler := func(buf []byte, _msgid string) error {
+		t.Log("received:", string(buf), "msgid:", _msgid)
+		received = string(buf)
+		msgid = _msgid
+		return nil
+	}
+	sub, err := NewExactlyOnceConsumer(log, js, "test", "test", "test", "test.*", handler)
+	assert.NoError(t, err, "failed to create consumer")
+	assert.NotNil(t, sub, "sub result was nil")
+	_msgid := fmt.Sprintf("%v", time.Now().Unix())
+	_, err = js.Publish("test.test", []byte("hi"), nats.MsgId(_msgid))
+	assert.NoError(t, err, "failed to publish")
+	time.Sleep(time.Millisecond * 100)
+	assert.Equal(t, "hi", received, "message didnt match")
+	assert.Equal(t, _msgid, msgid, "msgid didnt match")
+	sub.Close()
+	n.Close()
+	server.Shutdown()
 }
