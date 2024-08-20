b91dee0a2cefdf9cedd2ac44fc7b1ed10863d67c
Robin Diddams <rdiddams@shopmonkey.io>
2024-08-20 14:16:36 -0500

    add DecodeNatsMsg

--- nats/init.go
+++ nats/init.go
@@ -1,10 +1,13 @@
 package nats
 
 import (
+	"encoding/json"
 	"fmt"
 
 	gnats "github.com/nats-io/nats.go"
+	"github.com/shopmonkeyus/go-common/compress"
 	"github.com/shopmonkeyus/go-common/logger"
+	"github.com/vmihailenco/msgpack/v5"
 )
 
 // NewNats will return a new nats connections
@@ -26,4 +29,29 @@ func NewNats(log logger.Logger, name string, hosts string, credentials gnats.Opt
 	}
 	log.Debug("NATS ping rtt: %v, host: %s (%s)", d, nc.ConnectedUrl(), nc.ConnectedServerName())
 	return nc, nil
+}
+
+// DecodeNatsMsg will decode the nats message into the provided interface.
+func DecodeNatsMsg(msg *gnats.Msg, v interface{}) error {
+	encoding := msg.Header.Get("content-encoding")
+	gzipped := encoding == "gzip/json"
+	msgpacked := encoding == "msgpack"
+	var err error
+	data := msg.Data
+	if gzipped {
+		data, err = compress.Gunzip(data)
+	} else if msgpacked {
+		var o any
+		err = msgpack.Unmarshal(data, &o)
+		if err == nil {
+			data, err = json.Marshal(o)
+		}
+	}
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(data, v); err != nil {
+		return err
+	}
+	return nil
 }
