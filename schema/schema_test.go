5015099107c0eb562c5d81671acd9d0b2a04ffb6
Jeff Haynie <jhaynie@shopmonkey.io>
2024-09-23 19:44:41 -0500

    add test for API Fetcher

--- schema/schema_test.go
+++ schema/schema_test.go
@@ -3,7 +3,10 @@ package schema
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	cstr "github.com/shopmonkeyus/go-common/string"
@@ -41,6 +44,43 @@ func TestNewModelRegistry(t *testing.T) {
 		t.Fatal(err)
 	}
 	assert.False(t, fetcher.called)
+	if model.Public != m.Public || model.ModelVersion != m.ModelVersion {
+		t.Fatalf("expected model to be %v, got %v", m, model)
+	}
+}
+
+func TestNewModelRegistryWithAPIFetcher(t *testing.T) {
+	var m Model
+	m.Public = true
+	m.ModelVersion = "1234"
+	var called bool
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		called = true
+		assert.Equal(t, "/v3/schema/private/schema/table", r.URL.Path)
+		if r.URL.Query().Get("apikey") != "test" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, cstr.JSONStringify(m))
+	}))
+	defer ts.Close()
+	fetcher := NewAPIFetcher(ts.URL, "test")
+	r := NewModelRegistry(fetcher)
+	model, err := r.Get(context.Background(), "table")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.True(t, called)
+	if model.Public != m.Public || model.ModelVersion != m.ModelVersion {
+		t.Fatalf("expected model to be %v, got %v", m, model)
+	}
+	called = false
+	model, err = r.Get(context.Background(), "table")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.False(t, called)
 	if model.Public != m.Public || model.ModelVersion != m.ModelVersion {
 		t.Fatalf("expected model to be %v, got %v", m, model)
 	}
